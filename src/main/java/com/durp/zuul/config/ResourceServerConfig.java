d33564d45c7afd4cc0393c0b019170c0018974bf
durongpeng <durongpeng@aliyun.com>
2020-12-24 00:09:38 +0800

    commit

--- src/main/java/com/durp/zuul/config/ResourceServerConfig.java
+++ src/main/java/com/durp/zuul/config/ResourceServerConfig.java
@@ -1,8 +1,7 @@
 package com.durp.zuul.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -10,8 +9,13 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
 
-
-@Configuration
+// 获取code
+// http://localhost:9000/oauth/authorize?client_id=client&response_type=code&scope=all&redirect_uri=http://localhost:9000/r/r1
+// 获取token
+// http://localhost:9000/oauth/token?client_id=client&client_secret=secret&grant_type=authorization_code&code=h74kWB&redirect_uri=http://localhost:9000/r/r1
+// 检查token
+// http://localhost:9000/oauth/check_token?token=9032d158-d056-4ded-8072-c440f44a5a79
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
@@ -20,8 +24,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Bean
     public RemoteTokenServices tokenService() {
         RemoteTokenServices tokenService = new RemoteTokenServices();
-        tokenService.setClientId("order-client");
-        tokenService.setClientSecret("order-secret-8888");
+        tokenService.setClientId("client");
+        tokenService.setClientSecret("secret");
         tokenService.setCheckTokenEndpointUrl("http://localhost:9000/oauth/check_token");
         return tokenService;
     }
@@ -29,13 +33,15 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
         resources.tokenServices(tokenService());
+        resources.resourceId(RESOURCE_ID);
     }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/**").permitAll()
-                .antMatchers("/redis/*").permitAll();
+                .antMatchers("/**").access("#oauth2.hasScope('all')")
+                .and().csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        ;
     }
 
 
